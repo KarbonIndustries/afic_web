74140e974ac57dfa8fcc43c77d5169618bf25a7e
Shammel Lee <shammel@karbonnyc.com>
2011-07-01 12:05:53 -0400

    mini chef cooking schedule + schedule update

--- public/mini_chef_schedule.php
+++ public/mini_chef_schedule.php
@@ -8,17 +8,13 @@ closeHeader();
 <body>
 
 <div class="pad10">
-<span class="block size14 bold blue2 marginB5">March 28, 31, April 1 - Apple Cinnamon Brioche</span>
-<span class="block size14 bold blue2 marginB5">April 4, 7, 9 - Banana Caramel Tart</span>
-<span class="block size14 bold blue2 marginB5">April 11, 14, 16 - Thyme Parmesan Mini Cake</span>
-<span class="block size14 bold blue2 marginB5">April 18, 21 23 - Red Velvet Whoopie Pies</span>
-<span class="block size14 bold blue2 marginB5">April 25, 28, 30 - Tangerine Sable Cookies</span>
-<span class="block size14 bold blue2 marginB5">May 2,5,7 - Cheezy Pretzels</span>
-<span class="block size14 bold blue2 marginB5">May 9, 12, 14 - Raspberry Frozen Yogurt Mousse</span>
-<span class="block size14 bold blue2 marginB5">May 16, 19, 21 - Strawberry Shortcake Cookies</span>
-<span class="block size14 bold blue2 marginB5">May 23, 26, 28 - Honey Apple Galette</span>
-<span class="block size14 bold blue2 marginB5">May 30, June 2, 4 - Pizza Brioche</span>
-<span class="block size14 bold blue2 marginB5">June 6, 9, 11 - Blackberry Cornmeal Mini Cakes</span>
-<span class="block size14 bold blue2 marginB5">June 13, 16 - Exotic Fruit Discovery</span>
+<span class="block size14 bold blue2 marginB5">June 30th - Red White Blue Crepe Cups</span>
+<span class="block size14 bold blue2 marginB5">July 7th - Cherry Yogurt Mini Cakes</span>
+<span class="block size14 bold blue2 marginB5">July 14th - Summer Tomato Foccacia</span>
+<span class="block size14 bold blue2 marginB5">July 21st - Lemon Strawberry Newtons</span>
+<span class="block size14 bold blue2 marginB5">July 28th - Honey Peach Phyllo Hand Pies</span>
+<span class="block size14 bold blue2 marginB5">August 4th - Watermelon Pops</span>
+<span class="block size14 bold blue2 marginB5">August 11th - Cranraisin Sports Bread (Rye Bread)</span>
+<span class="block size14 bold blue2 marginB5">August 18th - Plum Tatin</span>
 </body>
 </html>
