796a8fd35053ea60644cfbd7b58453e056d7c0fe
Shammel Lee <shammel@karbonnyc.com>
2011-09-11 02:50:52 -0400

    fix: home page --> update: mini chef cooking schedule

--- public/classes.php
+++ public/classes.php
@@ -66,7 +66,7 @@ openContent($page);
 	<img class="padR10 padT10 padB10" src="photos/aam.jpg" />
 	<span class="subtitle1 gray1">After-school program <br />
 	Tuesday: 3:30pm - 4:30pm | 4-12 years<br />
-	<span class="size12 gray2">Drop In: $45 | Winter: $480 | Spring: $320 | Summer: $320 | Fall: $600</span></span>
+	<span class="size12 gray2">Drop In: $45 | Winter: $480 | Spring: $320 | Summer: $320</span></span>
     <p class="pgraph">Art, Adventures and More brings their art studio to The Art Farm. Children will learn techniques using acrylic paints, pastels, clay sculpting and other media. Classes will be led by two experienced artists who will educate through demonstrations and samples. Your child will be inspired and their imagination and creativity will flow. They will paint seascapes, underwater themes, landscapes, flora, and learn about the animals living in their natural habitats. There will also be an introduction to Master Artists of Impressionism, Expressionism, Contemporary Art, Caribbean Art and they will explore art across cultures. We connect the visual arts to other disciplines to interpret the world. Each child will take home their own "Masterpeice!"</p>
 
 	<div id="" class="floatR clearB">
@@ -96,7 +96,7 @@ openContent($page);
 	Thursday: 3:30pm - 4:30pm | 2-8 years mixed ages<br />
     Saturday: 9:00am - 10:00am | 2-8 years mixed ages<br />
 	<br />
-	<a class="rust mouseCursor" onclick="window.open('mini_chef_schedule.php','mcSchedule','width=400,height=280,left=100,top=100,scrollbars=yes');">View Cooking Schedule</a><br />
+	<a class="rust mouseCursor" onclick="window.open('mini_chef_schedule.php','mcSchedule','width=480,height=400,left=100,top=100,scrollbars=yes');">View Cooking Schedule</a><br />
 	<span class="size12 gray2">Drop In: $40 | Winter: $456 | Spring: $456 | Summer: $304 | Fall: $570</span></span>
 	<p class="pgraph">Get ready to mix, measure and create as Mini Chef brings their "Culinary Playground" to The Art Farm. The popular children's cooking program, Mini Chef helps kids of all ages develop a love of cooking through cooking classes and birthday parties with age appropriate menus. It's all about learning about good nutrition and creating good food through the hands-on cooking activities. Creating local and global cuisine, Mini Chef's head chef, Sylvie Berger, brings children on a taste bud voyage and cooking adventure. Create, make, eat, enjoy!</p>
 
