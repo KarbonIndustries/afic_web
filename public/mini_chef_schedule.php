5133047a40e90add3d346de937ebb63687d5ae5f
Shammel Lee <holler@shammellee.com>
2015-06-26 13:48:15 -0400

    update cooking menu

--- public/mini_chef_schedule.php
+++ public/mini_chef_schedule.php
@@ -8,18 +8,14 @@ closeHeader();
 <body>
 
 <div class="pad10">
-<span class="block size14 bold blue2 marginB5">Apr 2nd, 4th: Spring Vegetable Cheese Buns</span>
-<span class="block size14 bold blue2 marginB5">Apr 9th , 11th: Berry Rhubarb Pie Twists</span>
-<span class="block size14 bold blue2 marginB5">Apr 16th, 18th: Baby Spinach Tortilla Wrap</span>
-<span class="block size14 bold blue2 marginB5">Apr 23rd, 25th: Raspberry Curd Cookie Boats</span>
-<span class="block size14 bold blue2 marginB5">Apr 30th, May 2nd: My Little Green Cake (savory)</span>
-<span class="block size14 bold blue2 marginB5">May 7th, 9th: Mothers Day Strawberry Shortcake Cookies</span>
-<span class="block size14 bold blue2 marginB5">May 14th, 16th: Fresh Kale Tortilla Chips</span>
-<span class="block size14 bold blue2 marginB5">May 21st, 23rd: Matcha Madeleines</span>
-<span class="block size14 bold blue2 marginB5">May 28th, 30th: Spring Pea Blinis</span>
-<span class="block size14 bold blue2 marginB5">Jun 4th, 6th: Blackberry Mousse</span>
-<span class="block size14 bold blue2 marginB5">Jun 11th, 13th: Cheezy Avocado Pizza Bread</span>
-<span class="block size14 bold blue2 marginB5">Jun 18th, 20th: Cherry Vanilla Bagels</span>
+<span class="block size14 bold blue2 marginB5">July 2 - Cherry Berry Pop Tarts</span>
+<span class="block size14 bold blue2 marginB5">July 9 - Fresh Corn Whoopie Pies</span>
+<span class="block size14 bold blue2 marginB5">July 16 - Watermelon Cucumber Popsicles</span>
+<span class="block size14 bold blue2 marginB5">July 23 - Heirloom Tomato Polenta Pizza</span>
+<span class="block size14 bold blue2 marginB5">July 30 - Minty Fruit Spring Rolls with Strawberry Dip</span>
+<span class="block size14 bold blue2 marginB5">Aug 6 - Summer Squash Chickpea Pancakes</span>
+<span class="block size14 bold blue2 marginB5">Aug 13 - Peachy Plum Wontons</span>
+<span class="block size14 bold blue2 marginB5">Aug 20 - Guacamole &amp; Fresh Pita Chips</span>
 
 </body>
 </html>
