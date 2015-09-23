69122191e77d711557bfce829195a49abf700b2a
Shammel Lee <holler@shammellee.com>
2015-09-23 19:03:29 -0400

    update classes + drop in + index + cooking schedule + weekend programs

--- public/mini_chef_schedule.php
+++ public/mini_chef_schedule.php
@@ -8,14 +8,21 @@ closeHeader();
 <body>
 
 <div class="pad10">
-<span class="block size14 bold blue2 marginB5">July 2 - Cherry Berry Pop Tarts</span>
-<span class="block size14 bold blue2 marginB5">July 9 - Fresh Corn Whoopie Pies</span>
-<span class="block size14 bold blue2 marginB5">July 16 - Watermelon Cucumber Popsicles</span>
-<span class="block size14 bold blue2 marginB5">July 23 - Heirloom Tomato Polenta Pizza</span>
-<span class="block size14 bold blue2 marginB5">July 30 - Minty Fruit Spring Rolls with Strawberry Dip</span>
-<span class="block size14 bold blue2 marginB5">Aug 6 - Summer Squash Chickpea Pancakes</span>
-<span class="block size14 bold blue2 marginB5">Aug 13 - Peachy Plum Wontons</span>
-<span class="block size14 bold blue2 marginB5">Aug 20 - Guacamole &amp; Fresh Pita Chips</span>
+<span class="block size14 bold blue2 marginB5">Sept 10 &amp; 12 - Apple Pie Filo Cigars</span>
+<span class="block size14 bold blue2 marginB5">Sept 17 &amp; 19 - Butternut Mozzarella Twists</span>
+<span class="block size14 bold blue2 marginB5">Sept 24 &amp; 26 - Concorde Grape Pop Tarts</span>
+<span class="block size14 bold blue2 marginB5">Oct 1 &amp; 3 - Roasted Sweet Potato Fougasse</span>
+<span class="block size14 bold blue2 marginB5">Oct 8 &amp; 10 - Caramelized Pear Pie Pops</span>
+<span class="block size14 bold blue2 marginB5">Oct 15 &amp; 17 - Pumpkin Parmesan Gnocchi</span>
+<span class="block size14 bold blue2 marginB5">Oct 22 &amp; 24 - Cider Brioche</span>
+<span class="block size14 bold blue2 marginB5">Oct 29 &amp; 31 - Halloween Cookie Deco</span>
+<span class="block size14 bold blue2 marginB5">Nov 5 &amp; 7 - Cranberry Petit Beurre</span>
+<span class="block size14 bold blue2 marginB5">Nov 12 &amp; 14 - Cauliflower Gratin Buns</span>
+<span class="block size14 bold blue2 marginB5">Nov 19 &amp; 21 - Pumpkin Pie</span>
+<span class="block size14 bold blue2 marginB5">Nov 28 - Cheezy Wild Mushroom Pretzel Pies</span>
+<span class="block size14 bold blue2 marginB5">Dec 3 &amp; 5 - Salted Chocolate Sparkle Cookies</span>
+<span class="block size14 bold blue2 marginB5">Dec 10 &amp; 12 - Holiday Cookie Extravaganza</span>
+<span class="block size14 bold blue2 marginB5">Dec 17 &amp; 19 Gingerbread Pancakes</span>
 
 </body>
 </html>
