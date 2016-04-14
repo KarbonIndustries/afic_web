f47ea1df965a7ad2aea85226d855cb212c7904c3
Shammel Lee <holler@shammellee.com>
2016-04-14 02:49:46 -0400

    update: 160414

--- public/mini_chef_schedule.php
+++ public/mini_chef_schedule.php
@@ -10,63 +10,63 @@ closeHeader();
 <div class='pad10'>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jan 7 &amp; 9 </span>
-    <span class='size14'>Orange Kings Cake</span>
+    <span class="bold blue2">Apr 7 &amp; 9</span>
+    <span class='size14'>Red Tomato Brioche and Mozzarella</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jan 14th &amp; 16th</span>
-    <span class='size14'>Mashed Potato and Leek Monkey Bread</span>
+    <span class="bold blue2">Apr 14 &amp; 16</span>
+    <span class='size14'>Strawberry Cookie Tart</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jan 21st &amp; 23rd</span>
-    <span class='size14'>Roasted Pear Biscuits</span>
+    <span class="bold blue2">Apr 21 &amp; 23</span>
+    <span class='size14'>Artichoke Chickpea Crackers</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jan 28th &amp; 30th</span>
-    <span class='size14'>Carrot Soup Buns</span>
+    <span class="bold blue2">Apr 28 &amp; 30</span>
+    <span class='size14'>Mango Lemon Madeleine</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Feb 4th &amp; 6th</span>
-    <span class='size14'>Buttermilk Lemon Tarts</span>
+    <span class="bold blue2">May 5 &amp; 7</span>
+    <span class='size14'>Mother's Day - Raspberry Pink Cake</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Feb 11th &amp; 13th</span>
-    <span class='size14'>Root Vegetable Pretzels</span>
+    <span class="bold blue2">May 12 &amp; 14</span>
+    <span class='size14'>Blueberry Hand Pie</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Feb 18th &amp; 20th</span>
-    <span class='size14'>Mandarin Challah Twists</span>
+    <span class="bold blue2">May 19 &amp; 21</span>
+    <span class='size14'>Green Biscuits</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Feb 25th &amp; 27th</span>
-    <span class='size14'>Cauliflower Cheese Naan</span>
+    <span class="bold blue2">May 26 &amp; 28</span>
+    <span class='size14'>Blackberry Ricotta Cubes</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Mar 3rd &amp; 5th</span>
-    <span class='size14'>Banana Yeast Bread</span>
+    <span class="bold blue2">June 2 &amp; 4</span>
+    <span class='size14'>Fresh Corn Foccacia</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Mar 10th &amp; 12th</span>
-    <span class='size14'>Nut-Free Petit Pain Muesli</span>
+    <span class="bold blue2">June 9 &amp; 11</span>
+    <span class='size14'>Peach Pull-Apart Mini Breads</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Mar 17th &amp; 19th</span>
-    <span class='size14'>Strawberry Rhubarb Compote Mini Tart</span>
+    <span class="bold blue2">June 16 &amp; 18</span>
+    <span class='size14'>Father's Day - Cheezy Avocado Pretzels</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Mar 24th &amp; 26th</span>
-    <span class='size14'>Artichoke Bagels</span>
+    <span class="bold blue2">June 23 &amp; 25</span>
+    <span class='size14'>Cherry Pop Tarts</span>
   </div>
 
 </div>
