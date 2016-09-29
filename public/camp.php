0603b4a85412445d22b3959b9b659298415ca36b
Shammel Lee <holler@shammellee.com>
2016-09-29 16:04:39 -0400

    update: add columbus day camp

--- public/camp.php
+++ public/camp.php
@@ -18,10 +18,58 @@ define('NEW_REG_MSG',"<p class='newRegNote'>The Art Farm has a new registration 
 
 <div class="clearB">
 
+<!-- ===================== -->
+<!-- = COLUMBUS DAY CAMP = -->
+<!-- ===================== -->
+
+<div class="clearB">
+	<span id="halfDay" class="title2 blue2 clearB">Columbus Day Camp</span>
+	<span class="subtitle1 gray1">Monday, October 10th</span>
+		<span class="size12 gray2">
+			Full Day 9am-3pm | 4-8 years $125/day<br />
+			Half Day 9am-12pm | 3-4 years $75/day<br />
+		</span><br /><br />
+
+    <p class="pgraph">
+      In 1492 Columbus sailed the ocean blue. The Art Farm camp is offering
+      lots of activities for your children to do. Music, art, cooking, live
+      animal science lessons, outdoor play and more. Children will be split up
+      into age-appropriate groups.
+    </p>
+
+    <p class='pgraph'>
+      The Art Farm will be partnering with The Iken Science Academy and the
+      camp will be hosted at their school at 351 E. 74th St. between 1st Ave.
+      &amp; York Ave.
+    </p>
+
+	<div id="" class="floatR clearB">
+		<a id="btn1R" class="" href="https://campscui.active.com/orgs/TheArtFarminTheCity?season=1636101">
+		<div class="btn1RR"><span class="invisible">.</span></div><div class="btn1RC">&#187; Register!</div><div class="btn1RL"><span class="invisible">.</span></div>
+		</a>
+	</div>
+</div>
+
+<div class="marginT20 clearB">&nbsp;</div>
+
+<div class="clearB">
+  <p class='rust size16 alignC'>
+    Holiday Camp for the end of December will be posted very soon.<br />
+    Please subscribe to our email list for registration information.
+  </p>
+
+  <p class='rust size16 alignC'>
+    Summer Camp 2017 registration begins January 2nd.
+  </p>
+</div>
+
+<div class="marginT20 clearB">&nbsp;</div>
+
 <!-- =============== -->
 <!-- = SUMMER CAMP = -->
 <!-- =============== -->
 
+<!--
 <h1 class='alignC margin0' style='color:#B9C9DA'>
   The Art Farm in The City <span style='color:#FF3333'>Summer Camp</span>
 </h1>
@@ -203,6 +251,7 @@ lands to visit the dinosaurs and bounce back to present time.</p>
 our own Olympics games.</p>
 
 <div class="marginT20 clearB">&nbsp;</div>
+-->
 
 
 <!--end content-->
