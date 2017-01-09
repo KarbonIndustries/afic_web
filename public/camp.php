8c6bacf6a007b851e0bb471852a453df7f84998c
Shammel Lee <holler@shammellee.com>
2017-01-09 02:41:59 -0500

    add winter camp 2017 info

--- public/camp.php
+++ public/camp.php
@@ -3,7 +3,6 @@ require 'functions.php';
 $page = "Camp";
 openHeader($page);
 openSwfObjCode();
-embedFlash("flash/slideshow685400.swf", "slideshow3", 685, 400);
 closeSwfObjCode();
 closeHeader();
 drawMainMenu();
@@ -14,14 +13,92 @@ define('NEW_REG_MSG',"<p class='newRegNote'>The Art Farm has a new registration 
 ?>
 <!--begin content-->
 
-<div id="slideshow3"></div>
+<img src='images/winter_camp_header.jpg'/>
+<div class="marginT20 clearB">&nbsp;</div>
 
 <div class="clearB">
+
+<!-- =========== -->
+<!-- WINTER CAMP -->
+<!-- =========== -->
+<div class="clearB">
+  <span id="winterCamp" class="title2 blue2 clearB">Winter Camps 2017</span>
+  <span class="subtitle1 gray1">Martin Luther King Jr. Day Camp January 16th</span>
+  <span class="subtitle1 gray1">Presidents Day &amp; Winter Break Camp February 20th - 24th</span>
+  <span class="size12 gray2">Ages 3-8 years</span><br />
+  <span class="size12 gray2">Join us for one day or for everyday!</span><br /><br />
+
+  <span class="title2 blue2 clearB">Activities for all ages include</span>
+  <p class="pgraph">
+    Live animal science lessons where the children can meet and pet new animals
+    every day, arts &amp; crafts, cooking with our amazing chef, live rockin'
+    music with guitar and singin, science projects, games, and special events!
+  </p>
+
+  <p class="pgraph">
+    The Art Farm will be partnering with The Iken Science Academy and the camp
+    will be hosted at their school at 351 East 74th Street between 1st Avenue
+    &amp; 2nd Avenue.
+  </p>
+
+  <div class="clearB">
+    <span id="halfDay" class="size20 bold clearB" style='color:#FF3333'>Half Day Group</span>
+    <span class="subtitle1 gray1">MLK Day Jan 16th</span>
+    <span class="subtitle1 gray1">Presidents Day &amp; Winter Break February 20th - 24th</span>
+    <span class="subtitle1 gray1">(Register for on day or all of them)</span>
+    <span class="size12 gray2">9am - 12:00pm | 3-4 years | $75/day</span><br />
+    <span class="size12 gray2">Early Drop Off 8:30am - 9:00am | $15/day</span><br /><br />
+
+    <p class="pgraph">
+      We offer a half-day camp with a warm, nurturing program. Children spend
+      time learning about holding and caring for our animals. They create art
+      masterpieces, bake goodies, and sing their favorite songs to live guitar.
+      Lots of play, fun, and games to celebrate winter break. We will provide a
+      snack for your child and if your child is not fully potty-trained, please
+      send him or her in pull-ups. We close the group at 15 children with 3
+      qualified teachers. Children must be 3 years old to join the group.
+    </p>
+
+    <div class="clearB high20"></div>
+
+    <a id="" class="register_btn register_btn__now" href="https://campscui.active.com/orgs/TheArtFarminTheCity?season=1784502" target="_blank">Register Now!</a>
+  </div>
+
+  <div class="separator"></div>
+
+  <div class="clearB">
+    <span id="fullDay" class="size20 bold" style='color:#FF3333'>Full Day Groups</span>
+    <span class="subtitle1 gray1">Presidents Day &amp; Winter Break February 20th - 24th</span>
+    <span class="subtitle1 gray1">(Register for on day or all of them)</span>
+    <span class="subtitle1 gray1">9am - 3pm | 4-5 or 6-8 years | $125/day</span>
+    <span class="size12 gray2">$550 for full week of Winter Break Camp February 20th - 24th</span><br />
+    <span class="size12 gray2">(Discount is only available for full week at time of full week registration)</span><br />
+    <span class="size12 gray2">Early Drop Off 8:30am - 9:00am | $15/day</span><br />
+    <span class="size12 gray2">Extended Day 3:00pm - 5pm | $30/day</span><br /><br />
+
+    <p class="pgraph">
+      Our full-day campers participate in music, art, cooking, science, nature,
+      games, and live science animal lessons where the children will meet and
+      pet new animals every day. The children will engage in many age-
+      appropriate activities with lots of laughter and fun as we all clebrate
+      winter break. Please send your child to camp with a lunch that is nut-
+      free since we are a nut-free facility. We will provide the children with
+      a snack before the end of the day. We close each group at 15 children
+      with 3 qualified teachers.
+    </p>
+
+    <div class="clearB high20"></div>
+
+    <a id="" class="register_btn register_btn__now" href="https://campscui.active.com/orgs/TheArtFarminTheCity?season=1784502" target="_blank">Register Now!</a>
+  </div>
+</div>
+
+<div class="marginT20 clearB">&nbsp;</div>
+
 
 <!-- ===================== -->
 <!-- = COLUMBUS DAY CAMP = -->
 <!-- ===================== -->
-
 <!--
 <div class="clearB">
   <span id="halfDay" class="title2 blue2 clearB">Columbus Day Camp</span>
@@ -59,6 +136,7 @@ define('NEW_REG_MSG',"<p class='newRegNote'>The Art Farm has a new registration 
 <!-- = HOLIDAY CAMP = -->
 <!-- ================ -->
 
+<!--
 <span class="title2 blue2 clearB marginT20">Holiday Camp <?= date('Y')?></span>
 <span class="subtitle1 gray1">December 19 - 23 and 27 - 30</span>
 <span class="subtitle1 gray1">Join us for 1 day or all 9 days.</span>
@@ -150,6 +228,8 @@ define('NEW_REG_MSG',"<p class='newRegNote'>The Art Farm has a new registration 
 
 <div class="clearB high30"></div>
 <div class="clearB high30"></div>
+-->
+
 <!-- =============== -->
 <!-- = SUMMER CAMP = -->
 <!-- =============== -->
