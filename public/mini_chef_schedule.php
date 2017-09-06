61c83fb1ca87cc7758931fbe67a8019825353103
Shammel Lee <holler@shammellee.com>
2017-09-06 11:01:18 -0400

    Update cooking schedule

--- public/mini_chef_schedule.php
+++ public/mini_chef_schedule.php
@@ -10,68 +10,78 @@ closeHeader();
 <div class='pad10'>
 
   <div class='marginB5'>
-    <span class="bold blue2">Apr 6<sup>th</sup> &amp; 8<sup>th</sup></span>
-    <span class='size14'>Zucchini Falafel</span>
+    <span class="bold blue2">Sept 7<sup>th</sup> - 9<sup>th</sup></span>
+    <span class='size14'>Plum Galette</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Apr 13<sup>th</sup> &amp; 15<sup>th</sup></span>
-    <span class='size14'>Apple Danish Brioche</span>
+    <span class="bold blue2">Sept 14<sup>th</sup> - 16<sup>th</sup></span>
+    <span class='size14'>Wild Mushroom Bagels</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Apr 20<sup>th</sup> &amp; 22<sup>nd</sup></span>
-    <span class='size14'>Broccoli Flat Bread Grilled Cheese</span>
+    <span class="bold blue2">Sept 21<sup>st</sup> - 23<sup>rd</sup></span>
+    <span class='size14'>Grape Garibaldis</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Apr 27<sup>th</sup> &amp; 29<sup>th</sup></span>
-    <span class='size14'>Strawberry Mint Pocket Pies</span>
+    <span class="bold blue2">Sept 28<sup>th</sup></span>
+    <span class='size14'>Cauliflower Biscuits</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">May 4<sup>th</sup> &amp; 6<sup>th</sup></span>
-    <span class='size14'>Baby Spinach Pita Bread</span>
+    <span class="bold blue2">Oct 5<sup>th</sup> - 7<sup>th</sup></span>
+    <span class='size14'>Apple Pie</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">May 11<sup>th</sup></span>
-    <span class='size14'>Berry Biscuits</span>
+    <span class="bold blue2">Oct 12<sup>th</sup> - 14<sup>th</sup></span>
+    <span class='size14'>Butternut Soup Buns</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">May 13<sup>th</sup></span>
-    <span class='size14'>Special Mothers Day Stawberry Shortcake in a Cup</span>
+    <span class="bold blue2">Oct 19<sup>th</sup> - 21<sup>st</sup></span>
+    <span class='size14'>Pear Tatin</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">May 18<sup>th</sup> &amp; 20<sup>th</sup></span>
-    <span class='size14'>Spring Pea Pancakes</span>
+    <span class="bold blue2">Oct 26<sup>th</sup> - 28<sup>th</sup></span>
+    <span class='size14'>Halloween Cookie Deco</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">May 25<sup>th</sup></span>
-    <span class='size14'>Raspberries Plum Tart</span>
+    <span class="bold blue2">Nov 2<sup>nd</sup> - 4<sup>th</sup></span>
+    <span class='size14'>Leek and Potato Cakes</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jun 1<sup>st</sup> &amp; 3<sup>rd</sup></span>
-    <span class='size14'>Fresh Kale Tortilla Chips</span>
+    <span class="bold blue2">Nov 9<sup>th</sup> - 11<sup>th</sup></span>
+    <span class='size14'>Cranberry &amp; Cream Cheese Rolls</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jun 15<sup>th</sup></span>
-    <span class='size14'>Cherry Pie</span>
+    <span class="bold blue2">Nov 16<sup>th</sup> - 18<sup>th</sup></span>
+    <span class='size14'>Pumpkin Pie</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jun 17<sup>th</sup></span>
-    <span class='size14'>Special Father's Day Banana Caramel Cake</span>
+    <span class="bold blue2">Nov 30<sup>th</sup> - Dec 1<sup>st</sup></span>
+    <span class='size14'>Spaghetti Squash Focaccia</span>
   </div>
 
   <div class='marginB5'>
-    <span class="bold blue2">Jun 22<sup>nd</sup> &amp; 24<sup>th</sup></span>
-    <span class='size14'>Zucchini Cheese and Spring Onion Buns</span>
+    <span class="bold blue2">Dec 7<sup>th</sup> - 9<sup>th</sup></span>
+    <span class='size14'>Speculoos (Spice Cookies)</span>
+  </div>
+
+  <div class='marginB5'>
+    <span class="bold blue2">Dec 14<sup>th</sup> - 16<sup>th</sup></span>
+    <span class='size14'>Gingerbread Brioche</span>
+  </div>
+
+  <div class='marginB5'>
+    <span class="bold blue2">Dec 21<sup>st</sup> - 23<sup>rd</sup></span>
+    <span class='size14'>Holiday Cookie Deco</span>
   </div>
 
 </div>
