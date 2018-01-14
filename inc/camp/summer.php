9f95d179bbdc3eb57eec6d79c5d98bf6aaeb9bca
Shammel Lee <holler@shammellee.com>
2018-01-14 17:16:54 -0500

    Update Summer camp

--- inc/camp/summer.php
+++ inc/camp/summer.php
@@ -21,7 +21,7 @@ and half day camp experiences for children ages 3-9</p>
 
 <p class="pgraph">Get your city kid involved with nature this summer. The Art
 Farm summer camp engages your child in care and compassion of all living
-creatures.  Campers will meet live animals from our indoor petting zoo each
+creatures. Campers will meet live animals from our indoor petting zoo each
 day. Kids will get their hands dirty in our patio garden, experience sunshine
 on walks to the playground, play sports in our state-of-the-art gym facilities
 and new this year, campers will participate in building and engineering. The
@@ -56,6 +56,10 @@ offers a 10% sibling discount.</p>
 
 <div class='clearB high20'></div>
 
+
+<!-- ====================== -->
+<!-- = HAMSTERS + FISHIES = -->
+<!-- ====================== -->
 <div class="clearB">
   <span id="halfDay" class="size20 bold clearB" style='color:#FF3333'>Hamsters Half Day Camp</span>
   <span class="subtitle1 gray1">9AM - 12:30PM | 3-4 years</span>
@@ -69,12 +73,16 @@ offers a 10% sibling discount.</p>
 
   <div class='clearB high20'></div>
 
+  <a class='blue2 bold underline' href='data/summer_camp_schdule_2018.pdf'>See Daily Schedule</a>
+
+  <div class='clearB high20'></div>
+
   <p class="pgraph">Offering half-day camp in a warm, nurturing environment we
   make sure that separation is gentle and easy for children and caregivers.
   Campers will have animal science lessons and up-close encounters with our
   live animals daily. They will create art masterpieces, garden, cook in the
   kitchen, and will enjoy playing musical instruments in music with guitar and
-  singing.  Every day, our campers will get fresh air outside on the terrace,
+  singing. Every day, our campers will get fresh air outside on the terrace,
   play parachute games, participate in water play, and more. They will also
   have indoor soccer, yoga, and active games. Twice a week campers will enjoy
   special events so even if your child does a partial week they will get to
@@ -107,6 +115,10 @@ offers a 10% sibling discount.</p>
 
 <div class='clearB high20'></div>
 
+
+<!-- ============ -->
+<!-- = FROGGIES = -->
+<!-- ============ -->
 <div class="clearB">
   <span id="fullDay" class="size20 bold" style='color:#FF3333'>Froggies</span>
   <span class="subtitle1 gray1">4 years</span>
@@ -119,6 +131,10 @@ offers a 10% sibling discount.</p>
 
   <span class="size12 gray2">Extended options available a la carte</span><br />
   <span class="size12 gray2">Early Drop Off 8AM - 9AM | $20/day | Extended Day 3PM - 6PM | $40/day</span><br />
+
+  <div class='clearB high20'></div>
+
+  <a class='blue2 bold underline' href='data/summer_camp_schdule_2018.pdf'>See Daily Schedule</a>
 
   <div class='clearB high20'></div>
 
@@ -161,6 +177,10 @@ offers a 10% sibling discount.</p>
 
 <div class='clearB high20'></div>
 
+
+<!-- ================================ -->
+<!-- = PARROTS, GECKOS, CHINCHILLAS = -->
+<!-- ================================ -->
 <div class="clearB">
   <span class="size20 bold" style='color:#FF3333'>Parrots, Geckos, &amp; Chinchillas</span><br />
   <span class='gray1 bold'>Parrots: Entering Kindergarten | Geckos: Entering First Grade | Chinchillas: Entering 2<sup>nd</sup> - 4<sup>th</sup> Grade</span>
@@ -176,8 +196,12 @@ offers a 10% sibling discount.</p>
 
   <div class='clearB high20'></div>
 
+  <a class='blue2 bold underline' href='data/summer_camp_schdule_2018.pdf'>See Daily Schedule</a>
+
+  <div class='clearB high20'></div>
+
   <p class="pgraph">Our full-day campers are registered by age appropriate
-  groups based on the grade they are entering in Fall 2018.  Campers will
+  groups based on the grade they are entering in Fall 2018. Campers will
   participate in music, art, cooking, animal care, science, and gardening. They
   will also play sports, yoga and games, walk to the playground, and
   participate in swimming at the 92nd Street Y. During swim, campers will
@@ -189,12 +213,12 @@ offers a 10% sibling discount.</p>
   groups will participate in electives twice a week where they can choose
   activities such as jewelry-making, scrapbooking, woodworking, drama, sports,
   and engineering. Every week, we will focus on a different theme and the
-  children will enjoy special theme presentations on Tuesdays and Wednesdays so
-  everyone is included in the fun.  Please pack a nut-free lunch for your child
+  children will enjoy special theme presentations on Wednesday and Thursday so
+  everyone is included in the fun. Please pack a nut-free lunch for your child
   or buy one through our lunch program. All campers will receive a healthy
   snack including fresh fruit at the end of the day before pick up. Weekly
   activities and special events will be centered around weekly themes such as
-  Superhero Week and Story Book Land.  Campers enrolled in five days a week
+  Superhero Week and Adventure Land. Campers enrolled in five days a week
   will participate in all activities twice a week, two and three day campers
   will participate in swimming, petting zoo and special events once a week.</p>
 
@@ -255,7 +279,7 @@ offers a 10% sibling discount.</p>
 
   <li class='camp__list-item camp__list-item--weekly-themes'>
     <h3 style='margin-bottom:0'>
-      <span class=''>Week 7:</span> <span style='font-weight:normal;font-size:0.8em'>(August 6<sup>th</sup> - 10<sup>th</sup>)</span> Adventure Lnad
+      <span class=''>Week 7:</span> <span style='font-weight:normal;font-size:0.8em'>(August 6<sup>th</sup> - 10<sup>th</sup>)</span> Adventure Land
     </h3>
   </li>
 
